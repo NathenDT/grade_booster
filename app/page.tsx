e28cdb206ff360df05b09c75530ef745d325b489
Nathen dela Torre <nathendelatorre@gmail.com>
2024-10-12 23:54:55 -0700

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
 export default function App() {
-    return (
-        <div className="w-full">
-            
+  return (
+    <div className='flex'>
+      <div className=''>
+        <h1>Courses</h1>
+
+        <button className=''>INFO 200</button>
+        <button className=''>INFO 290</button>
+      </div>
+
+      <div>
+        <div>
+          <h1>Assignments</h1>
         </div>
-    )
+
+        <div>
+          <h1>Chat</h1>
+        </div>
+      </div>
+    </div>
+  )
 }
